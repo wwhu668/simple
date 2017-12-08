41c51368878b5a4b2a541c91801b5503a4b480d1
wwhu668 <wwhu668@163.com>
2017-12-08 16:07:17 +0800

    暂存

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,4 +10,9 @@ class UsersController extends Controller
     {
         return view('users.create');
     }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
 }
