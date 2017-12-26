2f3899db100147a3034c88288c96bce903f906aa
wwhu668 <wwhu668@163.com>
2017-12-26 12:26:05 +0800

    登陆和退出登陆

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -56,6 +56,7 @@ class UsersController extends Controller
             'password' => bcrypt($request->get('password')),
         ]);
 
+        \Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
